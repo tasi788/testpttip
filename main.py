3de9aa4bec055c28354200d7e01a9af2019c18c1
踢低吸 <tdc@tdccc.com.tw>
2021-07-28 07:12:40 +0000

    🐛

--- main.py
+++ main.py
@@ -7,3 +7,14 @@ ptt_bot.login(
     os.getenv('acc'),
     os.getenv('passwd'),
     kick_other_login=True)
+
+board = os.getenv('board')
+
+test_board_list = board.split(',')
+
+for test_board in test_board_list:
+    index = ptt_bot.get_newest_index(
+        PTT.data_type.index_type.BBS,
+        board=test_board
+    )
+    print(f'{test_board} 最新文章編號 {index}')
